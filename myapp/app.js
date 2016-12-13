bfa188439c90865aabb7928c869387f2aa37849b
David Crespo <davidcrespoarroyo@gmail.com>
2016-12-13 08:52:54 +0000

    added images

--- myapp/app.js
+++ myapp/app.js
@@ -7,9 +7,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var test1 = require('./routes/test1');
+// var index = require('./routes/index');
+// var users = require('./routes/users');
+// var test1 = require('./routes/test1');
 var hello_world = require('./routes/hello_world');
 
 var app = express();
@@ -31,27 +31,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/test1', test1);
+// app.use('/', index);
+// app.use('/users', users);
+// app.use('/test1', test1);
 app.use('/hello_world', hello_world);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+  // var err = new Error('Not Found');
+  // err.status = 404;
+  // next(err);
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
+// app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
 //
 //   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  // res.status(err.status || 500);
+  // res.render('error');
+// });
 
 module.exports = app;
